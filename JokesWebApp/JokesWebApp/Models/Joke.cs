86487b1b82c145128436b85dfcd43fc6260481d0
ADMIN <ADMIN@DESKTOP-USI49OI>
2022-07-13 14:13:44 -0700

    initial commit

---
+++ JokesWebApp/JokesWebApp/Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public Joke()
+        {
+
+        }
+
+        public virtual int Id { get; set; }
+        public virtual string? JokeQuestion { get; set; }
+        public virtual string? JokeAnswer { get; set; }
+    }
+}
